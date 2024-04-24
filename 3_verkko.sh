d1a54a3b9d452498450e0fe1e9b90f8e958d8184
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-04-24 15:09:20 +0700

    Add 3_verkko.sh

--- 3_verkko.sh
+++ 3_verkko.sh
@@ -9,5 +9,7 @@ source ~/.bashrc
 
 conda activate verkko
 verkko -d asm \
-	--hifi data/hifi/CM*.fastq.gz \
-	--nano data/nanopore/CM*.fastq.gz
+	--hifi data/hifi/CM_M_HIFI_None_reads.fastq.gz \
+	--nano data/nanopore/CM_M_nanopore_None_reads.fastq.gz \
+	--hic1 data/hic/CM_M_HIC_FWD.fastq.gz \
+	--hic2 data/hic/CM_M_HIC_REV.fastq.gz
